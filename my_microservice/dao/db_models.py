b2ec4bde51e9bc8cb97dea6aa1894f264657db23
KerstenBreuer <kersten-breuer@outlook.com>
2021-06-30 13:42:19 +0000

    add license and doc strings

--- my_microservice/dao/db_models.py
+++ my_microservice/dao/db_models.py
@@ -1,1 +1,16 @@
-# All database specific ORMs (for example SQLAlchemy) should be defined here
+# Copyright 2021 Universität Tübingen, DKFZ and EMBL
+# for the German Human Genome-Phenome Archive (GHGA)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Defines all database specific ORM classes (e.g. for SQLAlchemy)"""
--- tests/integration/fixtures/__init__.py
+++ tests/integration/fixtures/__init__.py
@@ -13,4 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Entrypoint of the package"""
+"""Fixtures that exclusively used in unit tests"""
