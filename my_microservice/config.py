11988cfbd46aef6a3d5d01dea5c6500f37c777c9
KerstenBreuer <kersten-breuer@outlook.com>
2021-07-13 02:17:26 +0000

    fix config prefix

--- my_microservice/config.py
+++ my_microservice/config.py
@@ -20,7 +20,7 @@ from ghga_service_chassis_lib.config import config_from_yaml
 from ghga_service_chassis_lib.api import ApiConfigBase
 
 
-@config_from_yaml(prefix="my-microservice")
+@config_from_yaml(prefix="my_microservice")
 class Config(ApiConfigBase):
     """Config parameters and their defaults."""
 
