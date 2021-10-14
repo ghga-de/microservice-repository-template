bd8db607a200e28f0e0de6ae13fc1ce3b69c463f
KerstenBreuer <kersten-breuer@outlook.com>
2021-10-14 18:22:02 +0000

    test

--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -20,23 +20,24 @@ from fastapi.testclient import TestClient
 
 from my_microservice.api import app
 
-# def test_index():
-#     """Test the index endpoint"""
 
-#     client = TestClient(app)
-#     response = client.get("/")
+def test_index():
+    """Test the index endpoint"""
 
-#     assert response.status_code == status.HTTP_200_OK
-#     assert response.text == '"Hello World."'
+    client = TestClient(app)
+    response = client.get("/")
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.text == '"Hello World."'
 
 
-# def test_greet():
-#     """Test the greet endpoint"""
+def test_greet():
+    """Test the greet endpoint"""
 
-#     name = "Friendly Tester"
+    name = "Friendly Tester"
 
-#     client = TestClient(app)
-#     response = client.get(f"/greet/{name}")
+    client = TestClient(app)
+    response = client.get(f"/greet/{name}")
 
-#     assert response.status_code == status.HTTP_200_OK
-#     assert name in response.json()["message"]
+    assert response.status_code == status.HTTP_200_OK
+    assert name in response.json()["message"]
