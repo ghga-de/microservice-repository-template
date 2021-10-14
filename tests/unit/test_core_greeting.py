34fb56f85d61240feb80b53551ef873c25220388
KerstenBreuer <kersten-breuer@outlook.com>
2021-10-14 18:25:31 +0000

    re-activate test

--- tests/unit/test_core_greeting.py
+++ tests/unit/test_core_greeting.py
@@ -25,27 +25,28 @@ from my_microservice.models import Greeting
 
 from .fixtures.greetings import greeting_expression_fixture
 
-# @pytest.mark.parametrize(
-#     "language,isinformal,exception_expected",
-#     [
-#         ("Greek", True, None),
-#         ("Greek", False, None),
-#         ("Croatian", True, ValueError),
-#         ("Croatian", False, None),
-#         ("Russian", False, ValueError),
-#     ],
-# )
-# def test_generate_greeting(
-#     language: str, isinformal: bool, exception_expected: Optional[Type[Exception]]
-# ):
-#     """Tests the generate_greeting function"""
-#     error_context: Any = (
-#         pytest.raises(exception_expected) if exception_expected else nullcontext()
-#     )
-#     with error_context:  # expect error if exception_expected
-#         with greeting_expression_fixture:  # overwrite the default GREETING_EXPRESSIONS
-#             greeting = generate_greeting(
-#                 name="Friendly Tester", language=language, isinformal=isinformal
-#             )
 
-#         assert isinstance(greeting, Greeting)
+@pytest.mark.parametrize(
+    "language,isinformal,exception_expected",
+    [
+        ("Greek", True, None),
+        ("Greek", False, None),
+        ("Croatian", True, ValueError),
+        ("Croatian", False, None),
+        ("Russian", False, ValueError),
+    ],
+)
+def test_generate_greeting(
+    language: str, isinformal: bool, exception_expected: Optional[Type[Exception]]
+):
+    """Tests the generate_greeting function"""
+    error_context: Any = (
+        pytest.raises(exception_expected) if exception_expected else nullcontext()
+    )
+    with error_context:  # expect error if exception_expected
+        with greeting_expression_fixture:  # overwrite the default GREETING_EXPRESSIONS
+            greeting = generate_greeting(
+                name="Friendly Tester", language=language, isinformal=isinformal
+            )
+
+        assert isinstance(greeting, Greeting)
