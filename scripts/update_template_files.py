39f5c66e2fc64d093bf838d341766f61d609cbab
Christoph Zwerschke <c.zwerschke@dkfz-heidelberg.de>
2023-01-16 19:15:17 +0100

    Wording
    Co-authored-by: Thomas Zajac <thomas-jakob.zajac@uni-tuebingen.de>

--- scripts/update_template_files.py
+++ scripts/update_template_files.py
@@ -236,7 +236,7 @@ def main(check: bool = False):
         if updated:
             echo_failure("Validating files from template failed.")
             sys.exit(1)
-        echo_success("Successfully validated the files from the template.")
+        echo_success("Successfully validated files from template.")
     else:
         echo_success(
             "The files from the template have been updated."
