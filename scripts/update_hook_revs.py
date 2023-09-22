13efb09e24bd7604d5d7b051809bd39c2061d8f2
TheByronHimes <TheByronHimes@gmail.com>
2023-09-22 08:19:31 +0000

    Split script main() into multiple functions.
    Use typer wrapper (cli.py)

--- scripts/update_hook_revs.py
+++ scripts/update_hook_revs.py
@@ -18,24 +18,19 @@
 """Script to ensure the pre-commit hook revs match what is installed."""
 import re
 import sys
+from functools import partial
 from pathlib import Path
+
+from script_utils import cli
 
 REPO_ROOT_DIR = Path(__file__).parent.parent.resolve()
 PRE_COMMIT_CFG_PATH = REPO_ROOT_DIR / ".pre-commit-config.yaml"
 LOCK_FILE_PATH = REPO_ROOT_DIR / "requirements-dev.txt"
 
 
-def main():
-    """Compare configured pre-commit hooks with the installed dependencies. For the set
-    that overlap (e.g. `black`, `mypy`, `ruff`, etc.), make sure the versions match.
-    If running with `--check`, exit with status code 1 if anything is outdated.
-    If running without `--check`, update `.pre-commit-config.yaml` as needed.
-    Any invalid flags are ignored.
-    """
-    args = sys.argv[1:]
-    running_pre_commit = "--check" in args
-
-    dependency = re.compile(r"([^=\s]+)==([^\s]*?)\s")
+def inspect_lock() -> dict[str, str]:
+    """Inspect the lock file to get the resolved dependencies"""
+    dependency_pattern = re.compile(r"([^=\s]+)==([^\s]*?)\s")
     dependencies = {}
 
     # Get the set of dependencies from the requirements-dev.txt lock file
@@ -43,51 +38,101 @@ def main():
         lines = lock_file.readlines()
 
     for line in lines:
-        match = re.match(dependency, line)
+        match = re.match(dependency_pattern, line)
         if match:
             package, version = match.groups()
             dependencies[package] = version
 
+    return dependencies
+
+
+def get_repl_value(match, dependencies: dict[str, str], outdated_hooks: list[str]):
+    """Look up pre-commit hook id in list of dependencies. If there's a match, update
+    return the version stored in the dictionary"""
+    ver, name = match.groups()
+    if name in dependencies:
+        new_ver = dependencies[name].strip()
+
+        # Use the v prefix if it was used before
+        if ver.startswith("v"):
+            new_ver = ver[0] + new_ver
+
+        # Make a list of what's outdated
+        if new_ver != ver:
+            msg = f"\t{name} (configured: {ver}, expected: {new_ver})"
+            outdated_hooks.append(msg)
+        return new_ver
+    return ver
+
+
+def get_config():
+    """Obtain the current pre-commit hook config from .pre-commit-config.yaml"""
+    with open(PRE_COMMIT_CFG_PATH, encoding="utf-8") as pre_commit_config:
+        return pre_commit_config.read()
+
+
+def process_config(dependencies: dict[str, str], config: str) -> tuple[str, list[str]]:
+    """Compare pre-commit config with lock file dependencies.
+
+    Create a modified copy of the existing config file contents with the hook versions
+    synchronized to the lock file dependencies.
+
+    Returns:
+        `new_config`: the updated/synchronized pre-commit config.
+
+        `outdated_hooks`: a list of any outdated hooks with version discrepancy details.
+    """
     outdated_hooks: list[str] = []
 
-    def get_repl_value(match):
-        """Look up pre-commit hook id in list of dependencies. If there's a match, update
-        return the version stored in the dictionary"""
-        ver, name = match.groups()
-        if name in dependencies:
-            new_ver = dependencies[name].strip()
-
-            # Use the v prefix if it was used before
-            if ver.startswith("v"):
-                new_ver = ver[0] + new_ver
-
-            # Make a list of what's outdated
-            if new_ver != ver:
-                msg = f"\t{name} (configured: {ver}, expected: {new_ver})"
-                outdated_hooks.append(msg)
-            return new_ver
-        return ver
-
     hook_rev = re.compile(r"([^\s\n]+)(?=\s*hooks:\s*- id: ([^\s]+))")
-    with open(PRE_COMMIT_CFG_PATH, encoding="utf-8") as pre_commit_config:
-        config = pre_commit_config.read()
 
-    new_config = re.sub(hook_rev, repl=get_repl_value, string=config)
+    new_config = re.sub(
+        hook_rev,
+        repl=partial(
+            get_repl_value, dependencies=dependencies, outdated_hooks=outdated_hooks
+        ),
+        string=config,
+    )
+
+    return new_config, outdated_hooks
+
+
+def update_config(new_config: str):
+    """Write `new_config` to .pre-commit-config.yaml"""
+    with open(PRE_COMMIT_CFG_PATH, "w", encoding="utf-8") as pre_commit_config:
+        pre_commit_config.write(new_config)
+        cli.echo_success(f"Updated '{PRE_COMMIT_CFG_PATH}'")
+
+
+def output_failure(outdated_hooks: list[str]):
+    """Notify the user that some pre-commit hooks are outdated, and list those hooks."""
+    cli.echo_failure("The following pre-commit hook versions are outdated:")
+    for hook in outdated_hooks:
+        print(hook)
+    print("Run 'scripts/update_hook_revs.py' to update")
+    sys.exit(1)
+
+
+def main(check: bool = False):
+    """Compare configured pre-commit hooks with the installed dependencies.
+
+    For the set that overlap (e.g. `black`, `mypy`, `ruff`, etc.), make sure the
+    versions match. If running with `--check`, exit with status code 1 if anything is
+    outdated. If running without `--check`, update `.pre-commit-config.yaml` as needed.
+    """
+
+    dependencies = inspect_lock()
+    config = get_config()
+    new_config, outdated_hooks = process_config(dependencies, config)
+
     if config != new_config:
-        if running_pre_commit:
-            print("The following pre-commit hook versions are outdated:")
-            for hook in outdated_hooks:
-                print(hook)
-            print("Run 'scripts/update_hook_revs.py' to update")
-            sys.exit(1)
+        if check:
+            output_failure(outdated_hooks)
         else:
-            # update the pre-commit config
-            with open(PRE_COMMIT_CFG_PATH, "w", encoding="utf-8") as pre_commit_config:
-                pre_commit_config.write(new_config)
-                print(f"Updated '{PRE_COMMIT_CFG_PATH}'")
+            update_config(new_config)
     else:
-        print("Pre-commit hooks are up to date.")
+        cli.echo_success("Pre-commit hooks are up to date.")
 
 
 if __name__ == "__main__":
-    main()
+    cli.run(main)
